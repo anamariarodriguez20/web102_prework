c3f20b4daf42d7987beaedb1307f295346a26541
Ana Maria Rodriguez <amr2343@columbia.edu>
2023-01-05 01:39:36 -0500

    Challenge 6: description container addition.

--- index.js
+++ index.js
@@ -155,12 +155,18 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const numUnfundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+}).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+let gamePlural = numUnfundedGames == 1 ? "game remains" : "games remain";
+const displayStr = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${totalGames - numUnfundedGames} games. Currently, ${numUnfundedGames} ${gamePlural} unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+let p = document.createElement('p');
+p.innerHTML = displayStr;
+descriptionContainer.append(p);
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
